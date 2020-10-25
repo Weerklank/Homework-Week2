c22f4f92e56c9a6afe94b9b2e9cd663971369903
Weerklank <Zoe.veggie@gmail.com>
2020-10-24 20:24:33 -0700

    getting it up and running

---
+++ build/precache-manifest.e16b884dbc777f608335bc4a23c717ad.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c2e037407fc3f68fc7fbf1f4ddebfb1",
+    "url": "/MyPortfolio/index.html"
+  },
+  {
+    "revision": "fcad268f493d7168f245",
+    "url": "/MyPortfolio/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "650b9b8b8138ad1a2166",
+    "url": "/MyPortfolio/static/css/main.64509627.chunk.css"
+  },
+  {
+    "revision": "fcad268f493d7168f245",
+    "url": "/MyPortfolio/static/js/2.68bb36ad.chunk.js"
+  },
+  {
+    "revision": "c937c4fa47a9982fa1ac3c3838ba40ff",
+    "url": "/MyPortfolio/static/js/2.68bb36ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "650b9b8b8138ad1a2166",
+    "url": "/MyPortfolio/static/js/main.6dd2480d.chunk.js"
+  },
+  {
+    "revision": "0121deccbbf268fe3b94",
+    "url": "/MyPortfolio/static/js/runtime-main.5cbe6f0c.js"
+  },
+  {
+    "revision": "8cd75955a2ec1b296147ef6fd24a5484",
+    "url": "/MyPortfolio/static/media/Alertify.8cd75955.png"
+  },
+  {
+    "revision": "f8c27d3036ca1c801abed8bc0f9dea90",
+    "url": "/MyPortfolio/static/media/Art1.f8c27d30.png"
+  },
+  {
+    "revision": "1c03a6df5e3e6a721717599409e2d932",
+    "url": "/MyPortfolio/static/media/Art2.1c03a6df.png"
+  },
+  {
+    "revision": "f494060ddca2796395905ea5b500a9e6",
+    "url": "/MyPortfolio/static/media/Art3.f494060d.png"
+  },
+  {
+    "revision": "fd8ab5ecbb573cb6fae0e04bcecec98d",
+    "url": "/MyPortfolio/static/media/Art4.fd8ab5ec.png"
+  },
+  {
+    "revision": "20dffcc4ce71e82e1357abea909febf3",
+    "url": "/MyPortfolio/static/media/Art5.20dffcc4.png"
+  },
+  {
+    "revision": "5eaa3f27302498b2049d7da597d35428",
+    "url": "/MyPortfolio/static/media/Art6.5eaa3f27.png"
+  },
+  {
+    "revision": "1ead4e677173504ca089d3ae5316dfbf",
+    "url": "/MyPortfolio/static/media/BodyDog.1ead4e67.png"
+  },
+  {
+    "revision": "11af86df130e64a7b5d20c889028e31f",
+    "url": "/MyPortfolio/static/media/CardBackground.11af86df.png"
+  },
+  {
+    "revision": "aaf71dfea72c8aa591c6d5c9ee1cd060",
+    "url": "/MyPortfolio/static/media/DailyCalendar.aaf71dfe.png"
+  },
+  {
+    "revision": "51f831f06f72a06d97262f64e8eca13f",
+    "url": "/MyPortfolio/static/media/ExtraBackground.51f831f0.png"
+  },
+  {
+    "revision": "8b491e3e23251e0b90ab67207e2bd157",
+    "url": "/MyPortfolio/static/media/FaceDog.8b491e3e.png"
+  },
+  {
+    "revision": "995efd13fb5d5cb452433fab4cb945de",
+    "url": "/MyPortfolio/static/media/HMDYKAC.995efd13.png"
+  },
+  {
+    "revision": "d487be44f1b49298188135c6b5037915",
+    "url": "/MyPortfolio/static/media/IMG_2259.d487be44.png"
+  },
+  {
+    "revision": "04173fd69bed7e0949eec82f1af60eff",
+    "url": "/MyPortfolio/static/media/PassGen.04173fd6.png"
+  },
+  {
+    "revision": "9690cb18c0a80d60aca27e65925cd42c",
+    "url": "/MyPortfolio/static/media/Tunado.9690cb18.png"
+  },
+  {
+    "revision": "89bffab6d7a359af23187836d7529381",
+    "url": "/MyPortfolio/static/media/WIP.89bffab6.png"
+  },
+  {
+    "revision": "78955921f8c03958020ca42c688d1896",
+    "url": "/MyPortfolio/static/media/marble.78955921.jpg"
+  }
+]);
